5f89f756d822ae6bb2486da8cb393d013a0e7789
AdityaPawase <adityapawase2000@gmail.com>
2024-07-01 20:33:18 +0530

    Analog Clock Added

---
+++ Analog-Clock/AnalogClock.js
@@ -0,0 +1,13 @@
+setInterval(()=>{
+   date=new Date();
+   htime=date.getHours();
+   mtime=date.getMinutes();
+   stime=date.getSeconds();
+   hrotation=30*htime+mtime/2;
+   mrotation=6*mtime;
+   srotation=6*stime;
+
+   document.querySelector('.hour').style.transform=`rotate(${hrotation}deg)`;
+   document.querySelector('.minute').style.transform=`rotate(${mrotation}deg)`;
+   document.querySelector('.seconds').style.transform=`rotate(${srotation}deg)`;
+},1000);
